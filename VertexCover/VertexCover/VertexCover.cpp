88bdcc2df7cce71ae1f7b42e1e58e6b8f7fbfea0
apechenezhsky <nextzucker@gmail.com>
2015-02-23 01:06:42 +0500

    Add graph reader from file.

--- VertexCover/VertexCover/VertexCover.cpp
+++ VertexCover/VertexCover/VertexCover.cpp
@@ -1,11 +1,10 @@
-// VertexCover.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
-
+#include "Graph.h"
+#include <tchar.h>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	Graph *graph = new Graph();
+	graph->ReadGraph("graph.txt");
 	return 0;
 }
-
