1d1088310684d00085957f86ca3005fcb8707ad3
apechenezhsky <nextzucker@gmail.com>
2015-03-25 22:12:20 +0500

    Added checking covers and generating random graph.

--- VertexCover/VertexCover/Graph.cpp
+++ VertexCover/VertexCover/Graph.cpp
@@ -15,29 +15,47 @@ Graph::Graph()
 {
 }
 
-
 Graph::~Graph()
 {
 }
-
-struct ARC {
-	bool isCovered;
-	int fromVertex, toVertex;
-};
 
 void Graph::ReadGraph(string inputFileName)
 {
 	cout << "Vertex Cover." << endl;
 	ifstream infile("graph.txt");
 	infile >> vertexCount;
-	for (int i = 0; i<vertexCount; i++)
+	for (int i = 0; i < vertexCount; i++)
 	{
-		vector<int> row;
+		vector<bool> row;
 		int edge;
-		for (int j = 0; j<vertexCount; j++)
+		for (int j = 0; j < vertexCount; j++)
 		{
 			infile >> edge;
-			row.push_back(edge);
+			if (edge == 0){
+				row.push_back(false);
+			}
+			else
+			{
+				row.push_back(true);
+			}
+		}
+		graph.push_back(row);
+	}
+}
+
+void Graph::Generate(int vertexCount, int falseFrequency){
+	cout << "Generate graph." << endl;
+	for (int i = 0; i < vertexCount; i++)
+	{
+		vector<bool> row(vertexCount);
+		for (int j = 0; j < vertexCount; j++)
+		{
+			if (rand() / RAND_MAX > falseFrequency){
+				row.push_back(true);
+			}
+			else{
+				row.push_back(false);
+			}
 		}
 		graph.push_back(row);
 	}
@@ -52,17 +70,19 @@ void Graph::FindVertexCover(){
 	}
 	currentVertexSetNumber = 1;
 	vertexSetCount = pow(2, vertexForCover.size());
+	int minCoverSize = vertexForCover.size();
 	int vertexSet;
 	while ((vertexSet = GetNextVertexSet())>0){
 		vector<int> currentCover = GetCoverForNumber(vertexForCover.size(), vertexSet);
 		cout << "Current setNumber is " + to_string(vertexSet) << endl;
-		string coverString = "Current cover is [ ";
-		for each (int i in currentCover)
-		{
-			coverString += to_string(i) + " ";
+		PrintCoverInfo(currentCover);
+		if (IsSetCovered(currentCover, arcsForCover, vertexForCover)){
+			if (currentCover.size() < minCoverSize){
+				minCoverSize = currentCover.size();
+			}
 		}
-		cout << coverString + "]" << endl;
 	}
+	cout << "Min cover " + to_string(minCoverSize) << endl;
 	cin.get();
 }
 
@@ -106,4 +126,41 @@ vector<int> Graph::GetCoverForNumber(int coverSize, int coverNumber){
 		}
 	}
 	return resultCover;
+}
+
+void Graph::PrintCoverInfo(vector<int> cover){
+	string coverString = "Current cover is [ ";
+	for each (int i in cover)
+	{
+		coverString += to_string(i) + " ";
+	}
+	cout << coverString + "]" << endl;
+}
+
+bool Graph::IsSetCovered(vector<int> checkingCover, vector<Graph::ARC> arcCover, vector<int> vertexCover){
+	CoverArcs(checkingCover, &vertexCover);
+	return !HasGraphArcs();
+}
+
+void Graph::CoverArcs(std::vector<int> checkingCover, std::vector<int>* vertexCover){
+	for each (int vertexInCover in checkingCover)
+	{
+		for each (int vertex in *vertexCover)
+		{
+			if (graph[vertexInCover][vertex]){
+				graph[vertexInCover][vertex] = false;
+				graph[vertex][vertexInCover] = false;
+			}
+		}
+	}
+}
+
+bool Graph::HasGraphArcs(){
+	for each (vector<bool> verteces in graph)
+	{
+		if (std::find(verteces.begin(), verteces.end(), true) == verteces.end()){
+			return true;
+		}
+	}
+	return false;
 }
--- VertexCover/VertexCover/Graph.h
+++ VertexCover/VertexCover/Graph.h
@@ -1,20 +1,31 @@
 #pragma once
 #include <string>
 #include <vector>
+
 class Graph
 {
-public:
-	Graph();
-	~Graph();
-	void ReadGraph(std::string);
-	void FindVertexCover();
+	struct ARC {
+		bool isCovered;
+		int fromVertex, toVertex;
+	};
 
-private:
-	std::vector <std::vector <int> > graph;
-	int vertexCount, currentVertexSetNumber, vertexSetCount;
-	int GetNeighbore(int vertexIndex, std::vector<int> vertexForCover);
-	void DeleteIsolateVertex(std::vector<int>*);
-	int GetNextVertexSet();
-	std::vector<int> GetCoverForNumber(int coverSize, int coverNumber);
+	public:
+		Graph();
+		~Graph();
+		void ReadGraph(std::string);
+		void FindVertexCover();
+		void Generate(int vertexCount, int frequency);
+
+	private:
+		std::vector <std::vector <bool> > graph;
+		int vertexCount, currentVertexSetNumber, vertexSetCount;
+		int GetNeighbore(int vertexIndex, std::vector<int> vertexForCover);
+		void DeleteIsolateVertex(std::vector<int>*);
+		int GetNextVertexSet();
+		std::vector<int> GetCoverForNumber(int coverSize, int coverNumber);
+		void PrintCoverInfo(std::vector<int> cover);
+		bool IsSetCovered(std::vector<int> CheckingCover, std::vector<ARC> arcCover, std::vector<int> vertexCover);
+		void CoverArcs(std::vector<int> vertexCover, std::vector<int>* verteces);
+		bool HasGraphArcs();
 };
 
